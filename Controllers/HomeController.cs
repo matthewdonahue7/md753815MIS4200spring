998750e5a96967bd439d700bda4d10556d7e4061
Matthew Donahue <md753815@ohio.edu>
2020-01-27 11:42:05 -0500

    Start modeling

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,14 +15,14 @@ namespace md753815MIS4200spring.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "My MIS4200 description";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Email: md753815@ohio.edu";
 
             return View();
         }
---
+++ Models/customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace md753815MIS4200spring.Models
+{
+    public class customer
+    {
+        public int customerID { get; set; }
+        public string customerFirstName { get; set; }
+        public string customerLastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime customerSince { get; set; }
+    }
+}
