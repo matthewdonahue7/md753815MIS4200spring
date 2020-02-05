6cc61846f0e3a4cf6c3f63ba091b49596e2cc905
Matthew Donahue <md753815@ohio.edu>
2020-02-05 11:14:07 -0500

    Finished

---
+++ Views/Pet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace md753815MIS4200spring.Views
+{
+    public class Pet
+    {
+        public int petID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime customerSince { get; set; }
+        public ICollection<officeVisit> officeVisit { get; set; }
+       // public string fullName
+       // {
+       //     get
+        //    {
+        //        return customerLastName + "," + customerFirstName;
+
+        //    }
+        ///}
+    }
+}
+
+
---
+++ Views/officeVisit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace md753815MIS4200spring.Views
+{
+    public class officeVisit
+    {
+        
+        public int officeVisitID { get; set; }
+        public string description { get; set; }
+        public DateTime officeVisitDate { get; set; }
+        // add any other fields as appropriate
+        //Order is on the "one" side of a one-to-many relationship with OrderDetail
+        //and we indicate that with an ICollection
+        //public ICollection<OrderDetail> OrderDetail { get; set; }
+        //Order is on the Many side of the one-to-many relation between Customer
+        //and Order and we represent that relationship like this
+        public int petID { get; set; }
+        public virtual Pet Pet { get; set; }
+    }
+}
